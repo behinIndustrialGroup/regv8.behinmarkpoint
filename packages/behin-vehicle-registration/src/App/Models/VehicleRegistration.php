4bd25dd5d6b6dc782dd6a8cd4de7abdea50081de
mkhodroo <info@behin.energy>
2024-09-07 19:36:17 +0330

    inti

---
+++ packages/behin-vehicle-registration/src/App/Models/VehicleRegistration.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace BehinVehicleRegistration\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VehicleRegistration extends Model
+{
+    public $table = 'vehicle_registrations';
+    protected $fillable = [
+        'unique_id','firstname', 'lastname', 'nin', 'phone', 'residence_state', 
+        'vehicle_manufacturer', 'vehicle_model', 'vehicle_year', 'vehicle_reg_state', 'vehicle_reg_number', 
+        'payment_file', 'price', 'authority', 'payment_status', 'payment_date'
+    ];
+}
